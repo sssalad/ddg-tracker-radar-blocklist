c1d75a5a6d6c6e4ade277069c95210244ffa1c48
sssalad <skeetskeetmafk@protonmail.com>
2025-01-04 16:34:13 -0800

    Added basic arguments

--- ddg-tr.py
+++ ddg-tr.py
@@ -1,12 +1,23 @@
 from pathlib import Path
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import sys
+
 from downloader import getDomains
 from searcher import Searcher
 from writer import writeDFtoList
 
-domainDirectory = Path.home() / ".ddgtrbl"
-domainCSV = domainDirectory / "domains.csv"
 
-listDirectory = Path.cwd() / "lists"
+# Set arguments
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument('-d', '--domaindirectory', default=(Path.home() / ".ddgtrbl"), help="Directory that domain data will be stored in")
+parser.add_argument('-l', '--listdirectory', default=(Path.cwd() / "lists"), help="Directory that lists will be written to")
+args = vars(parser.parse_args())
+
+
+# Set global variables based on arguments
+domainDirectory = args["domaindirectory"]
+domainCSV = domainDirectory / "domains.csv"
+listDirectory = args["listdirectory"]
 
 categories = ["Action Pixels", "Ad Fraud", "Ad Motivated Tracking", "Advertising", "Analytics" , 
               "Audience Measurement", "Badge", "CDN", "Embedded Content", "Federated Login", 
@@ -15,10 +26,21 @@ categories = ["Action Pixels", "Ad Fraud", "Ad Motivated Tracking", "Advertising
               "Third-Party Analytics Marketing", "Unknown High Risk Behavior"]
 
 def main():
-    domainDirectory.mkdir(exist_ok=True, parents=True)
-    listDirectory.mkdir(exist_ok=True, parents=True)
+    # Create the needed directories if they don't already exist
+    try:
+        domainDirectory.mkdir(exist_ok=True, parents=True)
+    except:
+        print("Error creating domain directory. Make sure file path is valid and writable")
+        sys.exit()
 
-    #getDomains('zip', str(domainDirectory))
+    try:
+        listDirectory.mkdir(exist_ok=True, parents=True)
+    except: 
+        print("Error creating list directory. Make sure file path is valid and writable")
+        sys.exit()
+
+    # Do actual stuff 
+    getDomains('zip', str(domainDirectory))
     search = Searcher(domainCSV)
     for category in categories:
         categoryDF = search.searchCategory(category)
